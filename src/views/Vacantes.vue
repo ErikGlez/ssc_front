4010956338b60d1315ea0136bc74e629ab3bdb60
Erik González <erik.gonzalez.ac@gmail.com>
2025-05-13 17:55:03 -0700

    Feat<Contrataciones> add contrato and error messages

--- src/views/Vacantes.vue
+++ src/views/Vacantes.vue
@@ -139,6 +139,11 @@
         </div>
     </div>
 
+     <div class="mgs_error" v-if="message">
+      <p>{{ message }}</p>
+    </div>
+   
+    
   </div>
 </template>
 
@@ -159,6 +164,7 @@ export default {
         formatCurrency,
         includesValue,
 
+        message:'',
         id: '',
         Puesto:'',
         Descripcion:'',
@@ -188,7 +194,7 @@ export default {
     }
   },
   methods:{
-    ...mapActions('vacantes', ['getAllInfo', 'deleteItem', 'addItem' , 'editItem']),
+    ...mapActions('vacantes', ['getAllInfoVacantes', 'deleteItem', 'addItem' , 'editItem']),
     setValues(item){
       const data = structuredClone(item)
       this.id = data.id
@@ -200,7 +206,7 @@ export default {
     async updateInfo(){
         this.regLoading = true
         this.process = 'Obteniendo vacantes'
-        await this.getAllInfo('Vacantes')
+        await this.getAllInfoVacantes('Vacantes')
         this.regLoading = false
         this.process = ''
     },
@@ -211,6 +217,11 @@ export default {
       this.Salario = ''
       this.Departamento = ''
       this.itemSel = null
+      this.id = ''
+    },
+
+    clearError: function () {
+        setTimeout(() => this.message = '', 2000);
     },
 
     async createItem(Puesto, Descripcion, Salario, Departamento){
@@ -218,7 +229,7 @@ export default {
       this.regLoading = true
       this.process = 'Creando vacante'
 
-      await this.addItem({
+      const response = await this.addItem({
         data: { Puesto, Descripcion, Salario: parseFloat(Salario), Departamento },
         table_name: 'Vacantes'
       })
@@ -228,6 +239,10 @@ export default {
       await this.updateInfo() 
       this.regLoading = false
       this.process = ''
+       if(!response?.id){
+        this.message = "Ha ocurrido un error al intentar agregar la vacante."
+        this.clearError()
+      }
 
     },
 
